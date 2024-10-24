530bce26a3d0d66d8a8f5ef4b0f9efc383dfe722
ill-soul077 <hassannaquib015@gmail.com>
2024-10-25 02:09:30 +0600

    added about

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define lmax LLONG_MAX
+
+
+// vector<ll> graph[N];
+// bool visited[N];
+// void dfs(ll vertex){
+//     /* Take action on vertex after entering the vertex */
+//     if(visited[vertex])return;
+//     visited[vertex] = true;
+//     for(ll child:graph[vertex]){
+//         /* Take action on child before entering the child node */
+//         dfs(child);
+//         /* Take action on child after existing the child node */
+//     }
+//     /* Take action on child before existing the vertex */
+// }
+
+void solve()
+{
+
+}
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
